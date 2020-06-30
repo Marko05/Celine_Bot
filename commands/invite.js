b4a17a376f5d3d36d4e09993e9bbed84bd389de5
Marko05 <Markoklincov781@gmail.com>
2020-06-30 21:16:31 +0200

    424

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ const embed = new Discord.MessageEmbed()
 
 .setTitle("Invite")
 .setColor("PURPLE")
-.setDescription("[Invite](https://discord.com/api/oauth2/authorize?client_id=724324879830220902&permissions=8&scope=bot)\n[Support Server](https://discord.gg/MCYwfU4)")
+.setDescription("[Invite Celine](https://discord.com/api/oauth2/authorize?client_id=724324879830220902&permissions=8&scope=bot)\n[Support Server](https://discord.gg/MCYwfU4)")
 .setTimestamp()
 
 msg.channel.send(embed)
